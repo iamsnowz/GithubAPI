b13f617f531159eee6a60e1896818c327b1cc991
iAmSnow <iamsnow@iAmSnows-MacBook-Pro.local>
2020-01-07 18:14:15 +0700

    add project

---
+++ GithubAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubAPI
+//
+//  Created by iAmSnow on 7/1/2563 BE.
+//  Copyright © 2563 iAmSnow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
